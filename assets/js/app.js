ef81fd07d3c30a6716bec029fff739252521c1ac
lalitverma01 <lalitverma2923@gmail.comm>
2025-02-04 20:39:49 +0530

    slider section complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,34 @@ const navbarbtn = () => {
     document.getElementById("rotates2").classList.toggle("rotate43")
     document.body.classList.toggle("overflow-hidden")
 }
+
+
+$('.happy-customer-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+            }
+        },
+
+
+    ]
+});
